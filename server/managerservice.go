ce09a1c7a97a10dfb7263f2e9a09c37d7d4a6cfe
g41797 <g41797@gmail.com>
2023-11-23 14:07:04 +0200

    Add list of handlers

--- server/managerservice.go
+++ server/managerservice.go
@@ -4,41 +4,24 @@ import (
 	"context"
 
 	"githib.com/g41797/grpcadapter/pb"
+	"github.com/memphisdev/memphis.go"
 )
 
 var _ pb.ManagerServiceServer = (*ManagerService)(nil)
 
-func (srv *ManagerService) connect() error {
-	return nil
-}
-
-func (srv *ManagerService) disconnect() error {
-	return nil
-}
-
-func (srv *ManagerService) createStation(req *pb.CreateStationRequest) error {
-	return nil
-}
-
 type ManagerService struct {
 	pb.UnimplementedManagerServiceServer
-	bc *brokerConnector
+	bc   *brokerConnector
+	proc []processRequest
 }
 
 func (srv *ManagerService) Manage(ctx context.Context, mr *pb.ManageRequest) (*pb.Status, error) {
 
+	srv.buildChainOfResp()
+
 	status := pb.Status{}
 
-	if srv.bc == nil {
-		bc, err := newBrokerConnector()
-		if err != nil {
-			*status.Text = err.Error()
-			return &status, err
-		}
-		srv.bc = bc
-	}
-
-	mc, err := srv.bc.connect()
+	mc, err := srv.connect()
 	if err != nil {
 		*status.Text = err.Error()
 		return &status, err
@@ -46,12 +29,50 @@ func (srv *ManagerService) Manage(ctx context.Context, mr *pb.ManageRequest) (*p
 
 	defer mc.Close()
 
-	if req := mr.GetCreatestation(); req != nil {
-		if err := srv.createStation(req); err != nil {
-			*status.Text = err.Error()
+	for _, prc := range srv.proc {
+		resp, status := prc(mr, mc)
+		if resp {
+			return status, nil
 		}
-		return &status, nil
 	}
 
-	return &pb.Status{}, nil
+	*status.Text = "not implemented"
+
+	return &status, nil
+}
+
+type processRequest func(req *pb.ManageRequest, mc *memphis.Conn) (responsible bool, status *pb.Status)
+
+func (srv *ManagerService) connect() (*memphis.Conn, error) {
+	if srv.bc == nil {
+		bc, err := newBrokerConnector()
+		if err != nil {
+			return nil, err
+		}
+		srv.bc = bc
+	}
+
+	mc, err := srv.bc.connect()
+	return mc, err
+}
+
+func (srv *ManagerService) buildChainOfResp() {
+	if srv.proc != nil {
+		return
+	}
+	srv.proc = append(srv.proc, srv.createStation)
+
+	//
+	// Rest of functions: delete. etc
+	//
+}
+
+func (srv *ManagerService) createStation(req *pb.ManageRequest, mc *memphis.Conn) (responsible bool, status *pb.Status) {
+
+	cmd := req.GetCreatestation()
+	if cmd != nil {
+		return true, &pb.Status{}
+	}
+
+	return false, nil
 }
--- server/managerservice_test.go
+++ server/managerservice_test.go
@@ -18,23 +18,27 @@ func TestManagerService_CreateStation(t *testing.T) {
 
 	client := pb.NewManagerServiceClient(conn)
 
-	for i := 0; i < 10; i++ {
-
-		mreq := pb.ManageRequest{
-			Data: &pb.ManageRequest_Createstation{
-				Createstation: &pb.CreateStationRequest{Station: &pb.Station{Name: "FirstStation"}}}}
-
-		ctx := context.Background()
-
-		status, err := client.Manage(ctx, &mreq)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		if len(status.GetText()) > 0 {
-			t.Error(status.GetText())
-		}
+	for i := 0; i < 1; i++ {
+		createStation(t, client)
 	}
+}
+
+func createStation(t *testing.T, client pb.ManagerServiceClient) {
+	mreq := pb.ManageRequest{
+		Data: &pb.ManageRequest_Createstation{
+			Createstation: &pb.CreateStationRequest{Station: &pb.Station{Name: "FirstStation"}}}}
+
+	ctx := context.Background()
+
+	status, err := client.Manage(ctx, &mreq)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(status.GetText()) > 0 {
+		t.Error(status.GetText())
+	}
+
 }
 
 func createManagerServiceServer() *grpc.Server {
